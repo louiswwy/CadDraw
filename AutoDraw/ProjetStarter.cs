4e4b4bdff175fa4eec438f735627e9c92413414e
louiswwy <louis664724128@hotmail.com>
2015-11-21 16:22:02 +0800

    初始化

---
+++ AutoDraw/ProjetStarter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Windows;
+
+//[assembly: ExtensionApplication(typeof(AutoDraw.ProjetStarter))]
+//[assembly: CommandClass(typeof(AutoDraw.ProjetStarter))]
+namespace AutoDraw
+{
+    public class ProjetStarter
+    {
+        Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+        MainInterface form;
+        [CommandMethod("AutoDrawer")]
+        public void starter()
+        {
+            if (form == null || form.IsDisposed)
+            {
+                ed.WriteMessage("AutoDrawer 功能已载入.");
+                form = new MainInterface();
+                Application.ShowModelessDialog(form);  //显示非模态对话框 
+                //Application.ShowModalDialog(form);  //显示非模态对话框 
+
+            }
+
+            else
+            {
+
+                form.Activate();
+            }
+        }
+    }
+}
